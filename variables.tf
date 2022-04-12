757f94e49ec303561931d99b6ae72321dfa33f14
Nick <49295025+nickkjolsing@users.noreply.github.com>
2022-04-12 11:00:02 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
-variable "linode_token" {
-  description = "Token used for Linode API access"
-}
+# variable "linode_token" {
+#   description = "Token used for Linode API access"
+# }
 
-variable "ubuntu_root_pass" {
-  description = "Root password for Linode instance"
-}
+# variable "ubuntu_root_pass" {
+#   description = "Root password for Linode instance"
+# }
 
 variable "nick_macbook_public_key" {
   description = "Nick's MacBook Air public key"
 }
 
-variable "linode_node_type" {
-  description = "Linode node type / plan"
-}
+# variable "linode_node_type" {
+#   description = "Linode node type / plan"
+# }
 
-variable "linode_node_region" {
-  description = "Region where node will be spwaned"
-}
+# variable "linode_node_region" {
+#   description = "Region where node will be spwaned"
+# }
 
-variable "linode_node_image" {
-  description = "Image used to spwan node"
-}
+# variable "linode_node_image" {
+#   description = "Image used to spwan node"
+# }
 
-variable "linode_tags" {
-  description = "Tags for node"
-}
+# variable "linode_tags" {
+#   description = "Tags for node"
+# }
 
-variable "linode_group" {
-  description = "Group that node will be a part of"
-}
+# variable "linode_group" {
+#   description = "Group that node will be a part of"
+# }
 
 variable "hetzner_token" {
   description = "Hetzner API Token"
